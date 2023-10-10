db37df5c1e1187f119d1412e3f53a02d10f13923
Thang Nguyen <tomn2009@gmail.com>
2023-10-09 22:21:38 -0500

    PRGM-2: add logic for simValues

--- programs/prgm-2/hw2.py
+++ programs/prgm-2/hw2.py
@@ -1,6 +1,6 @@
 import gensim.downloader as g1
 
-def simValues(model, key: str, countList: []):
+def simValues(model: g1.Word2VecKeyedVectors, key: str, countList: []):
     """ 
     Args:
         model: a Word2Vec model loaded from Gensim
@@ -10,9 +10,33 @@ def simValues(model, key: str, countList: []):
         A list of similarity values for the m-th nearest neighbors. If an item in
         countList is not an integer or is out of range, the corresponding value is -10000.0.
     """
-    NotImplementedError()
+    if not isinstance(model, g1.Word2VecKeyedVectors):
+        raise ValueError("The 'model' parameter must be a Gensim Word2Vec model.")
 
-def simValuesPct(model, key: str, countPctList: []):
+    if not isinstance(key, str):
+        raise ValueError("The 'key' parameter must be a string.")
+
+    if not all(isinstance(count, int) for count in countList):
+        raise ValueError("All elements in 'countList' must be integers.")
+    
+    similarity_values = []
+
+    if key not in model:
+        # If the key is not in the vocabulary, return -10000.0 for all counts.
+        return [-10000.0] * len(countList)
+
+    for count in countList:
+        if count <= 0 or count > len(model.vocab) - 1:
+            # If count is out of range, return -10000.0.
+            similarity_values.append(-10000.0)
+        else:
+            # Find the m-th nearest neighbor and calculate its similarity value.
+            most_similar = model.most_similar(key, topn=count)
+            similarity_values.append(most_similar[-1][1])  # Get the similarity value.
+
+    return similarity_values
+
+def simValuesPct(model: g1.Word2VecKeyedVectors, key: str, countPctList: []):
     """ 
     Args:
         model: a Word2Vec model loaded from Gensim
